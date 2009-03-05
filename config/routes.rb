bfa7d156264abeadf4c1de666595ae51e3c5193a
Wes Billman <wes.billman@gmail.com>
2009-03-04 22:24:21 -0800

    Added content to the settings page.  This includes adding a 'settings controller'

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 ActionController::Routing::Routes.draw do |map|
+
   map.resources :deck_cards
-
   map.resources :decks
-
   map.resources :clans
-
   map.resources :card_types
-
   map.resources :cards
-
-  map.root :controller => "home"
+  map.resources :setting
   map.resources :users
   map.resource  :session, :controller => 'session'
+  
+  map.settings '/settings', :controller => 'setting', :action => 'index'
   map.signup  '/signup', :controller => 'users', :action => 'new'
   map.login   '/login', :controller => 'session', :action => 'new'
   map.logout  '/logout', :controller => 'session', :action => 'destroy'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  
+  map.root :controller => "home"
 end
